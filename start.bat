6bcbfc37af8027bc9c712764ae56cbffff37fbbb
Flame_candle <nianyl_official@163.com>
2026-08-29 20:51:10 +0800

    upload change start.bat
    ohnononono

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
 set DEEPSEEK_API_KEY="YOUR API KEY"
-C:/Users/ASUS/AppData/Local/Programs/Python/Python314/python.exe qq_music_ai_player.py 2>nul
+"your python path" qq_music_ai_player.py 2>nul
